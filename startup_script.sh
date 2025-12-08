2b72e3bba676adb983a953df8afed85ee9257f55
WhoamiMiss <5822468+wlya@users.noreply.github.com>
2025-12-08 16:44:49 +0800

    Update startup_script.sh

--- startup_script.sh
+++ startup_script.sh
@@ -1,6 +1,7 @@
 sudo apt update
 sudo apt install -y git curl wget
 git clone https://github.com/wlya/ss /ss
+chmod a+x /ss/*
 SSKEY="$1" && echo "{\"server\":\"0.0.0.0\",\"server_port\":57611,\"local_address\":\"127.0.0.1\",\"local_port\":1080,\"password\":\"$SSKEY\",\"timeout\":600,\"method\":\"aes-256-gcm\"}" > /ss/ssconfig.json
 echo "ssconfig inited."
 bash /ss/init.sh
